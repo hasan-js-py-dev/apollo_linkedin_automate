1c2fb6c629f05f3c4e129393058820be2c32af5c
Hasan Rahman <104149095+hasan-js-py-dev@users.noreply.github.com>
2025-02-09 20:49:28 +0600

    Delete ApolloLinkedinAutomate directory

--- ApolloLinkedinAutomate/modules/apollo_list.py
+++
@@ -1,4 +0,0 @@
-# modules/apollo_list.py
-
-# The list name you want to ensure is used everywhere
-MY_DESIRED_LIST = "International Housewares Association"
--- ApolloLinkedinAutomate/modules/prompt_url.py
+++
@@ -1,8 +0,0 @@
-# modules/prompt_url.py
-
-def get_base_url():
-    """
-    Prompt the user for the LinkedIn Sales Navigator URL (starting page).
-    """
-    base_url = input("Enter the LinkedIn Sales Navigator URL (starting page):\n")
-    return base_url
